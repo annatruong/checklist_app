fc68fc4291ca71520014bee5a8311a3c15e317b5
annatruong <annatruong321@hotmail.com>
2019-12-14 13:47:38 +0000

    heroku deployment changes

--- checklist/__init__.py
+++ checklist/__init__.py
@@ -8,7 +8,7 @@ from flask_mail import Mail
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '2978c84723daace33d6964879c66f71c'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
